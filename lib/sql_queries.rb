122854b6bf2bf5b4bf25fc749488eede119628fd
kelly_maass <kmaass77627@gmail.com>
2025-03-17 11:11:56 -0500

    done

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -2,10 +2,10 @@
 
 
 
-def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
+ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 
-  "SELECT name, age FROM bears Where alive = 1 ORDER BY age ASC"
-end
+   "SELECT name, age FROM bears Where alive = 1 ORDER BY age ASC"
+ end
 
 def selects_oldest_bear_and_returns_name_and_age
   "SELECT bears.name, bears.age FROM bears ORDER BY age DESC LIMIT 1"
